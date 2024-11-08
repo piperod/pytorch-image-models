264f7a8586358f3ed6bb779379f820a593fe7932
piperod <ivan.rodriguez5@brown.edu>
2024-11-08 07:39:33 -0500

    adding alexmax, basically an alexnet but in the structure of hmax, trying to keep same parameters

---
+++ run_alexmax.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --time=160:00:00
+#SBATCH -p gpu --gres=gpu:2
+#SBATCH -n 4
+#SBATCH -N 1
+#SBATCH --mem=80GB
+#SBATCH -o alexmax_cl_0_ip_1.out
+#SBATCH -e alexmax_cl_0_ip_1.err
+#SBATCH --account=carney-tserre-condo
+#SBATCH -J alexmax_cl_0_ip_1_2
+
+
+module load anaconda/2023.09-0-7nso27y
+module load python/3.9.16s-x3wdtvt
+module load cuda
+
+source  /users/irodri15/data/irodri15/Hmax/hmax_pytorch/venv/bin/activate
+
+sh distributed_train.sh 2 train_skeleton.py \
+    --data-dir /gpfs/data/tserre/npant1/ILSVRC/ \
+    --dataset torch/imagenet \
+    --model alexmax \
+    --model-kwargs ip_scale_bands=1 classifier_input_size=25600 \
+    --cl-lambda 0\
+    --opt sgd \
+    -b 128 \
+    --epochs 90 \
+    --lr 1e-2 \
+    --weight-decay 5e-4 \
+    --sched step \
+    --momentum 0.9 \
+    --lr-cycle-decay 0.1 \
+    --decay-epochs 30 \
+    --warmup-epochs 0 \
+    --hflip 0.5\
+    --train-crop-mode rrc\
+    --input-size 3 227 227\
+    --experiment alexmax_cl_0_ip_1_227 \
+    --output /users/irodri15/data/irodri15/Hmax/pytorch-image-models/output/train/ \
--- timm/models/__init__.py
+++ timm/models/__init__.py
@@ -79,6 +79,7 @@ from .xception_aligned import *
 from .xcit import *
 from .HMAX import *
 from .alexnet import *
+from .ALEXMAX import * 
 
 from ._builder import build_model_with_cfg, load_pretrained, load_custom_pretrained, resolve_pretrained_cfg, \
     set_pretrained_download_progress, set_pretrained_check_hash
